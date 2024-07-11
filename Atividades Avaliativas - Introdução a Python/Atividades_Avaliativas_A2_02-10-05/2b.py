d9b37d2e6499d9102bf786836ced39a74b1cb054
0thferr <152410711+0thferr@users.noreply.github.com>
2024-07-11 13:19:50 -0300

    Python, Javascript, C.

---
+++ Atividades Avaliativas - Introdução a Python/Atividades_Avaliativas_A2_02-10-05/2b.py
@@ -0,0 +1,13 @@
+...
+200011514 Thais Ferreira
+...
+
+arq = open("integrantes_familia.txt","r")
+
+while True:
+    line = arq.readline()
+    if not line:
+        break
+    print(line)
+
+arq.close()
