f68fbdda12982fc16dae2ad1c6e649a45d234549
0thferr <152410711+0thferr@users.noreply.github.com>
2024-07-11 11:55:13 -0300

    Projetos Iniciais - Python & Javascript

---
+++ Atividades Avaliativas - Introdução a Python/Atividades_Avaliativas_A2_01-03-05/1c.py
@@ -0,0 +1,8 @@
+'''
+200011514 Thais Ferreira
+'''
+senha = 'senha123'
+tentativa = input("Digite a senha: ")
+while senha != tentativa:
+    tentativa = input("Digite a senha: ")
+print("Senha correta!")
---
+++ Atividades Avaliativas - Introdução a Python/Atividades_Avaliativas_A2_02-10-05/2a.py
@@ -0,0 +1,12 @@
+...
+200011514 Thais Ferreira
+...
+
+arq = open("integrantes_familia.txt","w")
+
+arq.write("Cida, 49\n")
+arq.write("Neno, 48\n")
+arq.write("Lucas, 24\n")
+arq.write("Rafael, 20")
+
+arq.close()
